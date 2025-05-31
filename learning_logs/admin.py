bf382e1b721f2f4ab19bd0ad99438ee9010f6636
git <bpdaut@outlook.com>
2025-05-31 14:55:55 -0500

    Doing a little homeworka dn following along to page 389

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Topic, Entry
+
+#The . in front of models tells Django to look in the same directory as admin
+
+admin.site.register(Topic)
+admin.site.register(Entry)
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,6 +6,28 @@ class Topic(models.Model):
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add = True)
 
-def __str__(self):
-    """Return a string representation of the model"""
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
+
+
+#Creating an Entry Model that can be nested under a Topic
+class Entry(models.Model):
+    """Something specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete = models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add = True)
+
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry."""
+        if len(self.text) > 50: 
+            return f"{self.text[:50]}..."
+        
+        #The ellipsis (...) is to remind the UI that we're not presenting the entire entry. Well. It shows anyway even if you have less than 50 characters so woops oh well.
+
+        else:
+            return f"{self.text}"
