90088993375fad8c5af4ef0b971777ce09bca907
git <bpdaut@outlook.com>
2025-06-13 19:45:49 -0500

    Doing TIY and tidying up my HTMLs and adding in security to make sure users only see their own topics and entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,15 +10,23 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
+
+@login_required
 def topics(request):
     """Show all topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner = request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all of its entries"""
     topic = Topic.objects.get(id = topic_id)
+    
+    #Make sure the topic belongs to the current user.
+    check_topic_owner(request, topic)
+    
     entries = topic.entry_set.order_by('-date_added')
     #THE LOWERCASE ENTRY IS REFERENCING THE ENTRY CLASS IN THE MODELS.PY FILE.
     #IF YOU WANT TO USE FOREIGN_KEYS PROPERLY AND GRAB MANY ITEMS ASSOCIATED TO A SINGLE PIZZA OR TOPIC FOR EXAMPLE
@@ -24,6 +34,8 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -36,7 +48,9 @@ def new_topic(request):
         #POST data submitted; process data
         form = TopicForm(data = request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit = False)
+            new_topic.owner = request.user
+            new_topic.save()
             #The save method writes this to the DB
             return redirect('learning_logs:topics')
             #I think this redirect is saying use the the topics function above which calls a new HTML page to redirect you to.
@@ -46,9 +60,15 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
+    
+    #Make sure the topic belongs to the current user before alowing new entries.
+    check_topic_owner(request, topic)
+    
     #Grabbing an individual topic
 
     if request.method != 'POST':
@@ -69,10 +89,15 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry"""
     entry = Entry.objects.get(id = entry_id)
     topic = entry.topic
+    
+    #Make sure the topic belongs to the current user.
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         #Initial request; pre-fill form with the current entry.
@@ -87,3 +112,9 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context) 
+
+
+def check_topic_owner(request, topic):
+    #Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
